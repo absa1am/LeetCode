7eaf83e1010880bb3f4d80d2cef96bc1ee66fbdc
Abdus Salam <salaam.mbstu@gmail.com>
2022-06-15 21:44:17 +0600

    Array Sorting

---
+++ MergeSortedArray.cs
@@ -0,0 +1,10 @@
+public class MergeSortedArraySolution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        for(int i = m, k = 0; i < nums1.Length; i++, k++)
+            nums1[i] = nums2[k];
+
+        Array.Sort(nums1);
+    }
+}
