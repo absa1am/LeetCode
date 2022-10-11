dc8c78945973a7d13c89fd9564296cae6eb93753
Abdus Salam <salaam.mbstu@gmail.com>
2022-10-11 14:08:00 +0600

    1523. Count Odd Numbers in an Interval Range

---
+++ ProgrammingSkills-I/CountOddNumbersInAnIntervalRange.cs
@@ -0,0 +1,10 @@
+public class CountOddNumbersInAnIntervalRangeSolution
+{
+    public int CountOdds(int low, int high)
+    {
+        if(low % 2 == 0) low++;
+        if(high % 2 == 0) high--;
+        
+        return (high - low) / 2 + 1;
+    }
+}
