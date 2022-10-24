9bbc25bf0e4dfc965419efecd8c6d0dbfa411322
Abdus Salam <salaam.mbstu@gmail.com>
2022-10-24 11:37:37 +0600

    283. Move Zeros

--- Easy/MoveZeros.cs
+++ Easy/MoveZeros.cs
@@ -4,21 +4,11 @@ public class MoveZerosSolution
     {
         for(int i = 0, j = 0; i < nums.Length; i++)
         {
-            if(nums[i] == 0)
+            if(nums[j] != 0)
             {
-                j = i + 1;
-                while(j < nums.Length)
-                {
-                    if(nums[j] != 0)
-                    {
-                        nums[i] = nums[j];
-                        nums[j] = 0;
-                        break;
-                    }
-                    j++;
-                }
-
-                if(j >= nums.Length) break;
+                nums[i] = nums[j];
+                if(i != j) nums[j] = 0;
+                i++;
             }
         }
     }
